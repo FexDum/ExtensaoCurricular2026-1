980b5c615fb1db928490886603e065c4eac8ef30
FexDum <131215435+FexDum@users.noreply.github.com>
2026-06-07 14:37:35 -0300

    Update HeaderFooter.js

--- src/js/HeaderFooter.js
+++ src/js/HeaderFooter.js
@@ -1,9 +1,9 @@
 async function carregarComponentes() {
     document.getElementById('header').innerHTML =
-        await (await fetch('/ExtensaoCurricular2026-1/header.html')).text();
+        await (await fetch('./header.html')).text();
 
     document.getElementById('footer').innerHTML =
-        await (await fetch('/ExtensaoCurricular2026-1/footer.html')).text();
+        await (await fetch('./footer.html')).text();
 }
 
 carregarComponentes();
